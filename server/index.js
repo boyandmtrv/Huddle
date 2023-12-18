ab8832d4bb1da98ad1a4909bc1b1a19b5ca21336
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-18 18:32:57 +0200

    implement join and sendMessage events in socket.io server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
+
 const router = require('./router');
 
 const PORT = 5000;
@@ -10,12 +12,44 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-    console.log('We have a new connection.');
+io.on('connect', (socket) => {
+
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) {
+            return callback(error)
+        };
+
+        socket.emit('message', {
+            user: 'admin',
+            text: `${user.name}, welcome to the room ${user.room}.`
+        });
+
+        socket.broadcast.to(user.room).emit('message', {
+            user: 'admin',
+            text: `${user.name} has joined the chat.`
+        });
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {
+            user: user.name,
+            text: message
+        });
+
+        callback();
+    });
 
     socket.on('disconnect', () => {
         console.log('User have left the chat.');
-    })
+    });
 });
 
 app.use(router);
