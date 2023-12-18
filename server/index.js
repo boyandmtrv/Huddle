fd55115411001b2fc778f0cb8ef6fda69f4694ee
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-18 15:13:42 +0200

    Added router and started implementation of socket.io

--- server/index.js
+++ server/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const router = require('./router');
+
 const PORT = 5000;
 const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection.');
+
+    socket.on('disconnect', () => {
+        console.log('User have left the chat.');
+    })
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log('Server is listening on port ' + PORT));
