2ad810183bcbc52f4504353673081138ab385ed8
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-18 16:06:01 +0200

    Connected socket.io

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { ENDPOINT } from "../../utils/const";
+
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
+
 const Chat = () => {
+
+    const location = useLocation();
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT, { transports : ['websocket'] });
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+    }, [ENDPOINT, location.search]);
+
     return (
         <h1>Chat</h1>
     )
