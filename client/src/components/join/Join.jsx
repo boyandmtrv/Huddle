4398525ac4a5ff2fbf2fe1f3b5a3525633d62087
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-18 15:29:36 +0200

    Added main file structure for join.jsx

--- client/src/components/join/Join.jsx
+++ client/src/components/join/Join.jsx
@@ -1,6 +1,61 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import './join.css'
+
 const Join = () => {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+
+    const onChangeNameInput = (e) => {
+        setName(e.target.value)
+    };
+
+    const onChangeRoomInput = (e) => {
+        setRoom(e.target.value)
+    };
+
+    const handleClick = ({ name, room }) => {
+        if (!name || !room) {
+            e.preventDefault();
+        } else {
+            return null;
+        }
+    };
+
+
     return (
-        <h1>Join</h1>
+        <div className="outer">
+            <div className="inner">
+                <h1 className="heading">Join</h1>
+                <div>
+                    <input
+                        type="text"
+                        className="join"
+                        placeholder="Name"
+                        onChange={onChangeNameInput}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        className="room"
+                        placeholder="Room"
+                        onChange={onChangeRoomInput}
+                    />
+                </div>
+                <Link
+                    onClick={handleClick}
+
+                    to={`/chat?name=${name}&room=${room}`}>
+                    <button
+                        className="button"
+                        type="submit"
+                    >Join</button>
+                </Link>
+            </div>
+        </div>
     )
 };
 
