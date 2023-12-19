5eca32328a48e8265a7084bf113093937a2f692b
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-19 10:55:15 +0200

    Added input component

---
+++ client/src/components/input/Input.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+
+const Input = ({
+    message,
+    setMessage,
+    sendMessage
+}) => {
+
+    const onMessageSend = (e) => {
+        setMessage(e.target.value)
+    };
+
+    const onLetterTyped = (e) => {
+        if (e.key === 'Enter') {
+            sendMessage(e);
+        } else {
+            return null;
+        };
+    };
+
+
+    return (
+        <form className="form">
+            <input
+                type="text"
+                className="input"
+                placeholder="Type a message"
+                value={message}
+                onChange={onMessageSend}
+                onKeyPress={onLetterTyped}
+            />
+
+            <button
+                className="sendButton"
+                onClick={(e) => sendMessage(e)}
+            >
+                Send
+            </button>
+        </form>
+    )
+};
+
+export default Input;
