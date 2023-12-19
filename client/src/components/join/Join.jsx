50ea86e5f5c45df214cafa1bb512683fc0850062
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-19 16:47:42 +0200

    Work continue on main page

--- client/src/components/join/Join.jsx
+++ client/src/components/join/Join.jsx
@@ -34,24 +34,31 @@ const Join = () => {
             console.log(err.message);
         }
     };
-
+    // bg-gradient-to-r from-zinc-950 via-zinc-800 to-zinc-950
     return (
-        <div className="outer flex lg:flex-row md:flex-row h-screen lg:w-full min-[320px]:flex-col items-center justify-center bg-gradient-to-r from-slate-950 via-slate-900 to-slate-800 relative">
-            <div className="inner backdrop-blur-md bg-opacity-10  bg-clip-padding backdrop-filter bg-slate-950 border-2 border-slate-900 lg:w-96 lg:h-96 md:w-96 md:h-96 mt-5 text-center flex flex-col justify-center items-center z-10 min-[320px]:h-64">
+        <div className="outer flex lg:flex-row md:flex-row h-screen lg:w-full min-[320px]:flex-col items-center justify-center  relative bg-zinc-950">
+            <Link to="#" className="absolute top-7 right-7 text-white flex items-center text-xl border-2 rounded-3xl p-2">
+                Learn more
+                <IoInformation className="ml-1" />
+            </Link>
+            <div style={{ color: '#ccc', WebkitTextFillColor: 'transparent', WebkitTextStrokeWidth: '2px' }} className="absolute top-[13%] left-5 ml-4 text-gray-500 lg:text-[90px] min-[826px]:text-[80px] min-[769px]:text-[80px]  min-[477px]:text-[50px] min-[320px]:text-[40px]">
+                Welcome to
+            </div>
+            <div className="inner backdrop-blur-md bg-opacity-10  bg-clip-padding backdrop-filter border-2 border-zinc-900 lg:w-[30%] lg:h-96 md:w-96 md:h-96 mt-5 text-center flex flex-col justify-center items-center z-10 min-[320px]:h-64">
                 <h1 className="heading text-2xl lg:text-3xl mb-4 text-white">Join a chat</h1>
                 <div className="mb-4">
                     <input
                         type="text"
-                        className="join py-2 px-4 rounded-md text-xl"
-                        placeholder="Name"
+                        className="join py-2 px-4 rounded-md text-xl bg-transparent border-2 border-zinc-900 text-white"
+                        placeholder="Enter your Name"
                         onChange={onChangeNameInput}
                     />
                 </div>
                 <div className="mb-4">
                     <input
                         type="text"
-                        className="room py-2 px-4 rounded-md text-xl"
-                        placeholder="Room"
+                        className="room py-2 px-4 rounded-md text-xl bg-transparent border-2 border-zinc-900 text-white"
+                        placeholder="Room Id / Room name"
                         onChange={onChangeRoomInput}
                     />
                 </div>
@@ -59,28 +66,28 @@ const Join = () => {
                     onClick={onClickChatJoin}
                     to={`/chat?name=${name}&room=${room}`}
                 >
-                    <button className="button mt-4" type="submit">
+                    <button className="button mt-4 text-white text-xl bg-zinc-900 w-24 h-[40px] rounded-3xl border border-white" type="submit">
                         Join
                     </button>
                 </Link>
             </div>
 
             <div className="inner backdrop-blur-md bg-opacity-10  bg-clip-padding backdrop-filter 
-             bg-slate-950 border-2 border-slate-900 lg:w-96 lg:h-96 md:w-96 md:h-96 mt-5 text-center flex flex-col justify-center items-center z-10 min-[320px]:h-64 lg:ml-5">
+              border-2 border-zinc-900 lg:w-[30%] lg:h-96 md:w-96 md:h-96 mt-5 text-center flex flex-col justify-center items-center z-10 min-[320px]:h-64 lg:ml-5 ">
                 <h1 className="heading text-2xl lg:text-3xl mb-4 text-white">Create a chat</h1>
                 <div className="mb-4">
                     <input
                         type="text"
-                        className="join py-2 px-4 rounded-md text-xl"
-                        placeholder="Name"
+                        className="join py-2 px-4 rounded-md text-xl bg-transparent border-2 border-zinc-900 text-white"
+                        placeholder="Enter your Name"
                         onChange={onChangeNameInput}
                     />
                 </div>
                 <div className="mb-4">
                     <input
                         type="text"
-                        className="room py-2 px-4 rounded-md text-xl"
-                        placeholder="Room"
+                        className="room py-2 px-4 rounded-md text-xl bg-transparent border-2 border-zinc-900 text-white"
+                        placeholder="Room Id / Room name"
                         onChange={onChangeRoomInput}
                     />
                 </div>
@@ -88,18 +95,17 @@ const Join = () => {
                     onClick={onClickChatJoin}
                     to={`/chat?name=${name}&room=${room}`}
                 >
-                    <button className="button mt-4" type="submit">
+                    <button className="button mt-4 text-white text-xl bg-zinc-900 w-24 h-[40px] rounded-3xl border border-white" type="submit">
                         Create
                     </button>
                 </Link>
             </div>
-            <div style={{ color: '#ccc', WebkitTextFillColor: 'transparent', WebkitTextStrokeWidth: '2px' }} className="absolute bottom-0 left-0 ml-4 text-gray-500 lg:text-[300px] z-0 min-[826px]:text-[250px] min-[769px]:text-[220px]  min-[477px]:text-[140px] min-[320px]:text-[100px] -webkit-text-stroke: 2px teal">
-                Huddle
+            <div style={{ color: '#ccc', WebkitTextFillColor: 'transparent', WebkitTextStrokeWidth: '2px' }} className="absolute bottom-0 left-0 ml-4 text-gray-500 lg:text-[300px] z-0 min-[826px]:text-[230px] min-[769px]:text-[220px]  min-[477px]:text-[140px] min-[320px]:text-[100px] -webkit-text-stroke: 2px teal">
+                Huddle.
             </div>
-            <div className="absolute lg:right-5 lg:top-[45%] mr-4 text-white text-2xl flex lg:flex-col lg:items-end  md:right-0 md:flex-row md:items-start sm:flex-row sm:items-start md:bottom-5 sm:right-5 sm:bottom-5 min-[320px]:flex-row min-[320px]:items-center min-[320px]:top-10">
-                <FiGithub className="lg:mb-10 md min-[320px]:mb-0 min-[320px]:ml-5" />
+            <div className="absolute lg:right-5 lg:top-[45%] mr-4 text-white text-2xl flex lg:flex-col lg:items-end  md:right-0 md:flex-row md:items-start sm:flex-row sm:items-start md:bottom-0 sm:right-5 sm:bottom-5 min-[320px]:flex-row min-[320px]:left-5 min-[320px]:top-10">
+                <FiGithub className="lg:mb-10 min-[320px]:mb-0 min-[320px]:ml-5" />
                 <FiLinkedin className="lg:mb-10 min-[320px]:mb-0 min-[320px]:ml-10" />
-                <IoInformation className="min-[320px]:ml-10" />
             </div>
         </div>
     );
