8205d66b16646d3b748691d2efbaf89fd881cc53
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-21 13:50:07 +0200

    Styling the JOIN conponent and adding react toast

--- client/src/components/join/Join.jsx
+++ client/src/components/join/Join.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { FiGithub, FiLinkedin } from "react-icons/fi";
-import { IoInformation } from "react-icons/io5";
 
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Join = () => {
     const [name, setName] = useState('');
@@ -20,14 +20,32 @@ const Join = () => {
 
     const onClickChatJoin = (e) => {
         try {
-            if (!name || !room) {
+            if (!name) {
                 e.preventDefault();
-                throw new Error('Both name and room are required!');
+                console.log('Name is required!');
+                toast.error('Name is required!');
+                return;
+            }
+
+            if (!room) {
+                e.preventDefault();
+                console.log('Room name / id is required!');
+                toast.error('Room name / id is required!');
+                return;
             }
 
             if (name.length < 3) {
                 e.preventDefault();
-                throw new Error('Name must be at least 3 characters long!');
+                console.log('Name must be at least 3 characters long!');
+                toast.error('Name must be at least 3 characters long!');
+                return;
+            }
+
+            if (room.length < 5) {
+                e.preventDefault();
+                console.log('Room name / id must be at least 5 characters long!');
+                toast.error('Room name / id must be at least 5 characters long!');
+                return;
             }
 
             return null;
@@ -35,18 +53,15 @@ const Join = () => {
             console.log(err.message);
         }
     };
-    // bg-gradient-to-r from-zinc-950 via-zinc-800 to-zinc-950
+
     return (
         <div className="outer flex lg:flex-row md:flex-row h-screen lg:w-full min-[320px]:flex-col items-center justify-center  relative bg-zinc-950">
-            <Link to="#" className="absolute top-7 right-7  text-white hover:bg-white hover:text-black hover:border-black transition ease-in-out flex items-center text-xl border-2 border-[#f0c195f6] rounded-3xl p-2">
-                Learn more
-                <IoInformation className="ml-1" />
-            </Link>
-            <div style={{ WebkitTextFillColor: 'transparent', WebkitTextStrokeWidth: '2px' }} className="welcome absolute top-[13%] left-5 ml-4 text-gray-500 lg:text-[90px] min-[826px]:text-[80px] min-[769px]:text-[80px]  min-[477px]:text-[50px] min-[320px]:text-[40px] text-[#b8b8b852]">
+
+            <div className="welcome absolute top-[13%] left-5 ml-4 text-gray-400 lg:text-[90px] min-[826px]:text-[80px] min-[769px]:text-[80px]  min-[477px]:text-[50px] min-[320px]:text-[40px] text-[#b8b8b852]">
                 Welcome to
             </div>
-            <div className="inner backdrop-blur-md bg-[#f0c1950a] bg-opacity-10  bg-clip-padding backdrop-filter border-2 border-[#f0c19557] lg:w-[28%] lg:h-[50%] md:w-96 md:h-96 mt-5 text-center flex flex-col justify-center items-center z-10 min-[320px]:h-64">
-                <h1 className="heading text-2xl lg:text-5xl mb-4 text-gray-300">Join a chat</h1>
+            <div className="inner backdrop-blur-md bg-[#f0c1950a] bg-opacity-10  bg-clip-padding backdrop-filter border-2 border-[#f0c19557] lg:w-[28%] lg:h-[50%] md:w-96 md:h-96 mt-5 text-center flex flex-col justify-center items-center z-10 min-[320px]:h-64 ">
+                <h1 className="heading text-2xl lg:text-5xl min-[320px]:text-3xl mb-4 text-gray-300">Join a chat</h1>
                 <div className="mb-4">
                     <input
                         type="text"
@@ -66,6 +81,7 @@ const Join = () => {
                 <Link
                     onClick={onClickChatJoin}
                     to={`/chat?name=${name}&room=${room}`}
+                    target="_blank" rel="noopener noreferrer"
                 >
                     <button className="button mt-4 text-white text-xl bg-zinc-900 w-24 h-[40px] rounded-3xl border border-white hover:bg-[#f0c195f6] hover:text-black hover:border-black transition ease-in-out" type="submit">
                         Join
@@ -74,7 +90,7 @@ const Join = () => {
             </div>
             <div className="inner bg-[#ffffff13] backdrop-blur-md bg-opacity-10  bg-clip-padding backdrop-filter 
               border-2 border-[#ffffff52] lg:w-[28%] lg:h-[50%] md:w-96 md:h-96 mt-5 text-center flex flex-col justify-center items-center z-10 min-[320px]:h-64 lg:ml-5 ">
-                <h1 className="heading text-2xl lg:text-5xl mb-4 text-[#f0c195f6]">Create a chat</h1>
+                <h1 className="heading text-2xl lg:text-5xl mb-4 text-[#f0c195f6] min-[320px]:text-3xl ">Create a chat</h1>
                 <div className="mb-4">
                     <input
                         type="text"
@@ -94,6 +110,7 @@ const Join = () => {
                 <Link
                     onClick={onClickChatJoin}
                     to={`/chat?name=${name}&room=${room}`}
+                    target="_blank" rel="noopener noreferrer"
                 >
                     <button className="button mt-4 text-white text-xl bg-zinc-900 w-24 h-[40px] rounded-3xl border border-white hover:bg-white hover:text-black hover:border-black transition ease-in-out" type="submit">
                         Create
@@ -104,9 +121,24 @@ const Join = () => {
                 Huddle.
             </div>
             <div className="absolute lg:right-5 lg:top-[45%] mr-4 text-white text-2xl flex lg:flex-col lg:items-end  md:right-0 md:flex-row md:items-start sm:flex-row sm:items-start md:bottom-0 sm:right-5 sm:bottom-5 min-[320px]:flex-row min-[320px]:left-5 min-[320px]:top-10">
-                <FiGithub className="lg:mb-10 min-[320px]:mb-0 min-[320px]:ml-5 text-white" />
-                <FiLinkedin className="lg:mb-10 min-[320px]:mb-0 min-[320px]:ml-10 text-white" />
+                <a href="https://github.com/boyandmtrv/" target="_blank" rel="noopener noreferrer">
+                    <FiGithub className="lg:mb-10 min-[320px]:mb-0 min-[320px]:ml-5 text-white" />
+                </a>
+                <a href="https://www.linkedin.com/in/boyan-dimitrov-4402b4179/" target="_blank" rel="noopener noreferrer">
+                    <FiLinkedin className="lg:mb-10 min-[320px]:mb-0 min-[320px]:ml-10 text-white" />
+                </a>
             </div>
+            <ToastContainer position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                pauseOnHover
+                theme="dark"
+            />
+
         </div>
     );
 };
